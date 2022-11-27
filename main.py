4b9d7a7cdea77495816e736b51c392e8bd21cbbf
Adenilson Silva <adenilsonsantos8891@gmail.com>
2022-11-27 11:15:37 -0300

    Tabuada
    atualizações

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 titulo = ('\033[1;34mTABUADA \033[1;33mPYTHON')
 print(titulo.center(40))
 print()
-nome = input('\033[mDigite Seu Nome: ')
+nome = input('\033[mDigite Seu Nome: ').title()
 print(f'Olá {nome}, Bem - Vindo a Tabuada Interativa!!')
 print("""[1] SOMA
 [2] SUBTRAÇÃO
@@ -13,5 +13,9 @@ if opcao == 1:
     resp = input('Tabuada Completa? [SIM/NÃO] ').strip().upper()[0]
     print()
     if resp in 'Ss':
+        titulo =('TABUADA COMPLETA')
+        print(titulo.center(152))
         for i in range(1, 11):
-            print(f'1 + {i:2} = {1 + i}', f'\t2 + {i:2} = {2+i}', f'\t3 + {i:} = {3+1}', f'\t4 + {i:2} = {4+i}', f'\t5 + {i:2} = {5+i}')
+            print(f'\n1 + {i:2} = {1 + i:2}', f'\t2 + {i:2} = {2 + i}', f'\t3 + {i:} = {3 + i}', f'\t4 + {i:2} = {4 + i}', 
+            f'\t5 + {i:2} = {5 + i}',f'\t6 + {i:2} = {6 + i}', f'\t7 + {i:2} = {i + 7}', f'\t8 + {i:2} = {8 + i}',
+            f'\t9 + {i:2} = {9 + i}', f'\t10 + {i:2} = {10 + i}')
