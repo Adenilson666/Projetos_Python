a116fd8f34d9f30f1b046d68d18824c2f09ae73e
Adenilson Silva <adenilsonsantos8891@gmail.com>
2022-12-28 20:58:39 -0300

    Tabuada Interativa

--- Tabuada.py
+++ Tabuada.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 def tabuada_soma():
-    resp = input('Tabuada Completa? [SIM/NÃO] ').strip().upper()[0]
+    resp = input('\033[mTabuada Completa? [SIM/NÃO] ').strip().upper()[0]
     print()
     sleep(1)
     if resp in 'Ss':
@@ -11,7 +11,7 @@ def tabuada_soma():
             print(f'\n1 + {i:2} = {1 + i:2}', f'\t2 + {i:2} = {2 + i:2}', f'\t3 + {i:2} = {3 + i:2}', f'\t4 + {i:2} = {4 + i:2}', 
             f'\t5 + {i:2} = {5 + i:2}',f'\t6 + {i:2} = {6 + i:2}', f'\t7 + {i:2} = {i + 7:2}', f'\t8 + {i:2} = {8 + i:2}',
             f'\t9 + {i:2} = {9 + i:2}', f'\t10 + {i:2} = {10 + i:2}')
-    else:
+    elif resp in 'Nn':
         print('Digite Dois Números especificos...')
         sleep(1)
         print()
@@ -21,9 +21,13 @@ def tabuada_soma():
         sleep(1)
         print()
         print(f'{num} + {num_2} = {num + num_2} ')
+    else:
+        print('\033[1;31mErro!! Digite novamente!!')
+        print()
+        tabuada_soma()
 
 def tabuada_subtracao():
-    resp = input('Tabuada Completa? [SIM/NÃO] ').strip().upper()[0]
+    resp = input('\033[mTabuada Completa? [SIM/NÃO] ').strip().upper()[0]
     print()
     sleep(1)
     if resp in 'Ss':
@@ -33,7 +37,7 @@ def tabuada_subtracao():
             print(f'\n1 - {i:2} = {1 - i:2}', f'\t2 - {i:2} = {2 - i:2}', f'\t3 - {i:2} = {3 - i:2}', f'\t4 - {i:2} = {4 - i:2}', 
             f'\t5 - {i:2} = {5 - i:2}',f'\t6 - {i:2} = {6 - i:2}', f'\t7 - {i:2} = {i - 7:2}', f'\t8 - {i:2} = {8 - i:2}',
             f'\t9 - {i:2} = {9 - i:2}', f'\t10 - {i:2} = {10 - i:2}')
-    else:
+    elif resp in 'Nn':
         print('Digite Dois Números especificos...')
         sleep(1)
         print()
@@ -43,9 +47,13 @@ def tabuada_subtracao():
         sleep(1)
         print()
         print(f'{num} - {num_2} = {num - num_2} ')
+    else:
+        print('\033[1;31mErro!! Digite novamente!!')
+        print()
+        tabuada_subtracao()
 
 def tabuada_multiplicacao():
-    resp = input('Tabuada Completa? [SIM/NÃO] ').strip().upper()[0]
+    resp = input('\033[mTabuada Completa? [SIM/NÃO] ').strip().upper()[0]
     print()
     sleep(1)
     if resp in 'Ss':
@@ -55,7 +63,7 @@ def tabuada_multiplicacao():
             print(f'\n1 x {i:2} = {1 * i:2}', f'\t2 x {i:2} = {2 * i:2}', f'\t3 x {i:2} = {3 * i:2}', f'\t4 x {i:2} = {4 * i:2}', 
             f'\t5 x {i:2} = {5 * i:2}',f'\t6 x {i:2} = {6 * i:2}', f'\t7 x {i:2} = {i * 7:2}', f'\t8 X {i:2} = {8 * i:2}',
             f'\t9 x {i:2} = {9 * i:2}', f'\t10 x {i:2} = {10 * i:3}')
-    else:
+    elif resp in 'Nn':
         print('Digite Dois Números especificos...')
         sleep(1)
         print()
@@ -65,9 +73,13 @@ def tabuada_multiplicacao():
         sleep(1)
         print()
         print(f'{num} x {num_2} = {num * num_2} ')
+    else:
+        print('\033[1;31mErro!! Digite novamente!!')
+        print()
+        tabuada_multiplicacao()
 
 def tabuada_divisao():
-    resp = input('Tabuada Completa? [SIM/NÃO] ').strip().upper()[0]
+    resp = input('\033[mTabuada Completa? [SIM/NÃO] ').strip().upper()[0]
     print()
     sleep(1)
     if resp in 'Ss':
@@ -77,7 +89,7 @@ def tabuada_divisao():
             print(f'\n1 : {i:2} = {1 / i:.2f}', f'\t2 : {i:2} = {i / 2:.2f}', f'\t3 : {i:2} = {i / 3:.2f}', f'\t4 : {i:2} = {i / 4:.2f}', 
             f'\t5 : {i:2} = {5 / i:.2f}',f'\t6 : {i:2} = {6 / i:.2f}', f'\t7 : {i:2} = {i / 7:.2f}', f'\t8 : {i:2} = {i / 8:.2f}',
             f'\t9 : {i:2} = {9 / i:.2f}', f'\t10 : {i:2} = {10 / i:.2f}')
-    else:
+    elif resp in 'Nn':
         print('Digite Dois Números especificos...')
         sleep(1)
         print()
@@ -87,8 +99,10 @@ def tabuada_divisao():
         sleep(1)
         print()
         print(f'{num} : {num_2} = {num / num_2} ')
-
-
+    else:
+        print('\033[1;31mErro!! Digite novamente!!')
+        print()
+        tabuada_divisao()
 
 def main():
     
@@ -113,7 +127,7 @@ def main():
     elif opcao == 4:
         tabuada_divisao()
     else:
-        print('\033[1;31mErro!! Tente Novamente!!')
+        print('\033[1;31mErro!! Digite Novamente!!')
         print()
         main()
 
